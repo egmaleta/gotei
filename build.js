aa45435a99985a4851b183893f1c912763691d99
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-09-16 09:38:14 -0400

    build lib for bun and node

--- build.js
+++ build.js
@@ -1,13 +1,24 @@
 import { rm } from "node:fs/promises";
 import dts from "bun-plugin-dts";
 
+async function build(target, minify, genTypes) {
+	const targetName = target === "browser" ? "" : `.${target}`;
+
+	await Bun.build({
+		entrypoints: ["./src/gotei.ts"],
+		outdir: "./dist",
+		target,
+		format: "esm",
+		minify,
+		plugins: genTypes ? [dts()] : [],
+		naming: `[dir]/[name]${targetName}.[ext]`,
+	});
+}
+
 await rm("./dist", { recursive: true });
 
-await Bun.build({
-	entrypoints: ["./src/index.ts"],
-	outdir: "./dist",
-	target: "browser",
-	format: "esm",
-	minify: true,
-	plugins: [dts()],
-});
+await Promise.allSettled([
+	build("browser", true, true),
+	build("bun", false, false),
+	build("node", false, false),
+]);

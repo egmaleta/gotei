f3540abfa3e7e28f4fa404c6815fcacd1e9d30d6
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-09-21 16:17:57 -0400

    build commonjs

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: ["src/index.ts", "src/runtime.ts", "src/state.ts"],
-			formats: ["es"],
-			fileName: (_, name) => `${name}.js`,
+			formats: ["es", "cjs"],
+			fileName: (format, name) => `${name}.${format === "cjs" ? format : "js"}`,
 		},
 		minify: false,
 	},

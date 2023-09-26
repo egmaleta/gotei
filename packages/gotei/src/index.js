4d3c70bc1e82395cc84d9cfd9568699ffd18d67f
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-09-25 21:36:31 -0400

    organize gotei package

---
+++ packages/gotei/src/index.js
@@ -0,0 +1,2 @@
+export * from "@goteijs/core/state";
+export * from "@goteijs/core";
--- packages/gotei/vite.config.ts
+++ packages/gotei/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	build: {
 		lib: {
-			entry: ["src/index.ts"],
+			entry: ["src/index.js"],
 			formats: ["es", "umd"],
 			name: "gotei",
 			fileName: (format) => `gotei.${format === "es" ? "esm" : format}.js`,

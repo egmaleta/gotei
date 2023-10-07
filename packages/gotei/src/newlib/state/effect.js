e7ac34799e8007662182480738389fe05190bd93
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-10-07 10:00:43 -0400

    impl state

---
+++ packages/gotei/src/newlib/state/effect.js
@@ -0,0 +1,16 @@
+import { define } from "./oop";
+import { CONTEXT } from "./context";
+
+export function Effect(callback, isUIEffect) {
+	this.callback = callback;
+	this.isUIEffect = isUIEffect;
+	this.update(null);
+}
+
+define(Effect, {
+	update(ctx) {
+		CONTEXT.push(this);
+		this.callback(ctx);
+		CONTEXT.pop();
+	},
+});
---
+++ packages/gotei/src/newlib/state/index.ts
@@ -0,0 +1,39 @@
+import { CONTEXT } from "./context";
+import { Effect } from "./effect";
+import { Signal as _Signal, ArraySignal } from "./signal";
+
+export type SignalGetter<T> = () => T;
+export type SignalSetter<T> = T extends Array<any>
+	? {
+			set(value: T): void;
+	  }
+	: {
+			set(value: T): void;
+			set(f: (old: T) => T): void;
+	  };
+export type Signal<T> = SignalGetter<T> & SignalSetter<T>;
+
+export function signal<T>(value: T): Signal<T> {
+	// @ts-ignore
+	const s = Array.isArray(value) ? new ArraySignal(value) : new _Signal(value);
+
+	return Object.assign(s.get.bind(s), {
+		set: s.set.bind(s),
+	});
+}
+
+export function effect(callback: () => any) {
+	new Effect(callback, false);
+}
+
+export function ref<T extends Node>() {
+	return signal<T | null>(null);
+}
+
+export function isRefReady<T extends Node>(ref: Signal<T | null>) {
+	return ref() !== null;
+}
+
+export const untrack = (CONTEXT as any).untrack.bind(CONTEXT) as <T>(
+	signalish: () => T,
+) => T;

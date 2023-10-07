71054dfb36514897f9fa4668bd3e9017febbdd36
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-10-07 10:14:38 -0400

    impl h function and tags proxy to create html elements

---
+++ packages/gotei/src/newlib/runtime/h.ts
@@ -0,0 +1,185 @@
+import { Effect } from "../state/effect";
+import { Gotei } from "./ns";
+import {
+	CSS_VAR_PREFIX,
+	EVENT_LISTENER_PREFIX,
+	addEventListener,
+	cls2Tokens,
+	setAttribute,
+	setCSSProp,
+	setCSSVar,
+	flatten,
+	OrArray,
+} from "./utils";
+
+type MountFunction = {
+	(parent: ParentNode, ...args: any[]): any;
+};
+
+type HTMLElementMountFunction<T extends Gotei.Tag> = {
+	(parent: ParentNode): HTMLElementTagNameMap[T];
+};
+
+type Child =
+	| MountFunction
+	| Node
+	| string
+	| number
+	| boolean
+	| undefined
+	| null;
+
+type AnyProps = Record<string, any>;
+
+export function h<T extends Gotei.Tag, P extends AnyProps>(
+	tag: T,
+	props: Gotei.Attrs<T> & P,
+	children: Child[],
+): HTMLElementMountFunction<T> {
+	const el = document.createElement(tag);
+
+	const {
+		bindThis,
+		bindValue,
+		use,
+		classList,
+		classRecord,
+		styleRecord,
+		...rest
+	} = props;
+
+	for (const [name, prop] of Object.entries(rest)) {
+		if (name.startsWith(EVENT_LISTENER_PREFIX)) {
+			addEventListener(el, name.slice(EVENT_LISTENER_PREFIX.length), prop);
+		} else if (typeof prop !== "function") {
+			setAttribute(el, name, prop);
+		} else {
+			new Effect(() => {
+				setAttribute(el, name, prop());
+			}, true);
+		}
+	}
+
+	for (const child of children) {
+		if (child instanceof Node) {
+			el.appendChild(child);
+		} else if (typeof child === "string" || typeof child === "number") {
+			el.appendChild(document.createTextNode(`${child}`));
+		}
+		// TODO: handle mountfunction children
+	}
+
+	if (bindValue) {
+		const isNumber = typeof bindValue() === "number";
+
+		new Effect(() => {
+			setAttribute(el, "value", bindValue());
+		}, true);
+		addEventListener(el, "input", (ev: any) => {
+			const value = ev.currentTarget.value;
+			// @ts-ignore
+			bindValue.set(isNumber ? Number.parseFloat(value) : value);
+		});
+	}
+
+	if (classList) {
+		for (const cls of classList) {
+			if (typeof cls !== "function") {
+				el.classList.add(...cls2Tokens(cls));
+			} else {
+				let tokens: string[] = [];
+
+				new Effect(() => {
+					el.classList.remove(...tokens);
+					tokens = cls2Tokens(cls());
+					el.classList.add(...tokens);
+				}, true);
+			}
+		}
+	}
+
+	if (classRecord) {
+		for (const [cls, ok] of Object.entries(classRecord)) {
+			const tokens = cls2Tokens(cls);
+			if (tokens.length === 0) continue;
+
+			if (typeof ok !== "function") {
+				ok && el.classList.add(...tokens);
+			} else {
+				new Effect(() => {
+					if (ok()) {
+						el.classList.add(...tokens);
+					} else {
+						el.classList.remove(...tokens);
+					}
+				}, true);
+			}
+		}
+	}
+
+	if (styleRecord) {
+		for (const [prop, value] of Object.entries(styleRecord)) {
+			const setFunction = prop.startsWith(CSS_VAR_PREFIX)
+				? setCSSVar
+				: setCSSProp;
+
+			if (typeof value !== "function") {
+				setFunction(el, prop, value);
+			} else {
+				new Effect(() => {
+					setFunction(el, prop, value());
+				}, true);
+			}
+		}
+	}
+
+	return (parent: ParentNode) => {
+		parent.appendChild(el);
+
+		// @ts-ignore
+		bindThis?.set(el);
+
+		if (use) {
+			if (Array.isArray(use)) {
+				for (const f of use) f(el as any);
+			} else {
+				use(el as any);
+			}
+		}
+
+		return el;
+	};
+}
+
+type Tags = {
+	[T in Gotei.Tag]: {
+		<P extends AnyProps>(
+			props: Gotei.Attrs<T> & P,
+			...children: OrArray<Child>[]
+		): HTMLElementMountFunction<T>;
+		(...children: OrArray<Child>[]): HTMLElementMountFunction<T>;
+	};
+};
+
+export const tags = new Proxy(
+	{},
+	{
+		get(_, tag: any) {
+			return (...args: any[]) => {
+				if (args.length > 0) {
+					const head = args[0];
+					if (
+						typeof head === "object" &&
+						head &&
+						!Array.isArray(head) &&
+						!(head instanceof Node)
+					) {
+						return h(tag, head, [...flatten(args.slice(1))]);
+					}
+				}
+
+				return h(tag, {}, [...flatten(args)]);
+			};
+		},
+	},
+) as Tags;

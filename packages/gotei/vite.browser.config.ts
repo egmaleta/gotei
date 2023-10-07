b1b4ad7475f557d35581211dd28fd7c805c1b791
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-10-07 09:58:21 -0400

    rename browser bundle entry

--- packages/gotei/vite.browser.config.ts
+++ packages/gotei/vite.browser.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	build: {
 		lib: {
-			entry: ["src/gotei.js"],
+			entry: ["./src/browser.js"],
 			formats: ["es", "umd"],
 			name: "gotei",
-			fileName: (format, name) =>
-				`${name}.${format === "es" ? "esm" : format}.js`,
+			fileName: (format) => `gotei.${format === "es" ? "esm" : format}.js`,
 		},
 		minify: true,
 		emptyOutDir: false,

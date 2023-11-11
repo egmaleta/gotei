d476e87031af73c2258d8747f709b664bd073a84
Eduardo García Maleta <egmaleta.secondary@proton.me>
2023-11-11 09:49:17 -0500

    build only esm

--- packages/gotei/vite.config.ts
+++ packages/gotei/vite.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: ["src/index.ts", "src/runtime.ts", "src/state.ts", "src/ssr.ts"],
-      formats: ["es", "cjs"],
-      fileName: (format, name) => `${name}.${format === "cjs" ? format : "js"}`,
+      formats: ["es"],
+      fileName: (_, name) => `${name}.js`,
     },
     minify: false,
     rollupOptions: {

88cca8274aef90847b3bb6011ec9c9294dfeb08f
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-02-10 10:31:17 -0500

    add function creation

---
+++ src/function.ts
@@ -0,0 +1,47 @@
+import { derived, signal } from "./state";
+import { getNamedVars } from "./store";
+
+const TRACKED_PATTERN = /\$(\w+)/g;
+const UNTRACKED_PATTERN = /&(\w+)/g;
+
+function normalizeCode(code: string) {
+  return code
+    .replace(TRACKED_PATTERN, (_, name) => `${name}.v`)
+    .replace(UNTRACKED_PATTERN, (_, name) => `${name}.uv`);
+}
+
+function createFunction(
+  element: Element,
+  code: string,
+  params: string[] = [],
+  args: any[] = [],
+) {
+  const body = `"use strict"; ${normalizeCode(code)}`;
+
+  const namedVars = getNamedVars(element);
+  params = namedVars.map(([name, _]) => name).concat(params);
+  args = namedVars.map(([_, v]) => v).concat(args);
+
+  return new Function(...params, body).bind(element, ...args);
+}
+
+function createSignal(element: Element, expr: string) {
+  const f = createFunction(element, `return ${expr};`);
+
+  if (TRACKED_PATTERN.test(expr)) {
+    return derived(f);
+  }
+
+  return signal(f());
+}
+
+function createComputation(element: Element, expr: string) {
+  const f = createFunction(element, `return ${expr};`);
+
+  if (TRACKED_PATTERN.test(expr)) {
+    return derived(f);
+  }
+  return f;
+}
+
+export { createFunction, createSignal, createComputation };

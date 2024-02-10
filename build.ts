88fe05bb0522f79885150039fd0a5797d0bb1ef3
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-02-10 14:56:56 -0500

    build minified version too

--- build.ts
+++ build.ts
@@ -6,9 +6,24 @@ await build({
       entry: ["src/index.ts"],
       formats: ["umd"],
       name: "gotei",
-      fileName: () => `gotei.js`,
+      fileName: () => "gotei.js",
     },
     target: "ES2015",
     minify: false,
+    emptyOutDir: true,
+  },
+});
+
+await build({
+  build: {
+    lib: {
+      entry: ["src/index.ts"],
+      formats: ["umd"],
+      name: "gotei",
+      fileName: () => "gotei.min.js",
+    },
+    target: "ES2015",
+    minify: true,
+    emptyOutDir: false,
   },
 });

34a57125d932bf274ecc5bda5f65d91df0908c6d
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-02-12 12:58:03 -0500

    reimplement data handler

--- src/attr-handler/data.ts
+++ src/attr-handler/data.ts
@@ -1,18 +1,55 @@
-import { createFunction, createSignal } from "../function";
+import { effect } from "..";
+import { createComputation, createSignal } from "../function";
 import { setVar } from "../store";
 import { attr } from "./attr";
 
 const DATA_ATTR_PREFIX = attr("data:");
 
-function handle(element: HTMLElement, attrSuffix: string, expr: string) {
-  const name = attrSuffix;
+function getItem(storage: Storage, key: string, as: string) {
+  const value = storage.getItem(key);
 
-  if (name.startsWith("$")) {
-    const s = createSignal(element, expr);
-    setVar(element, name.slice(1), s);
-  } else {
-    const f = createFunction(element, `return ${expr};`);
-    setVar(element, name, f());
+  if (value !== null) {
+    switch (as) {
+      case "string":
+        return value;
+      case "number":
+        return Number.parseFloat(value);
+      case "boolean":
+        return value === "true";
+    }
+  }
+}
+
+function handle(element: HTMLElement, attrSuffix: string, expr: string) {
+  const names = attrSuffix.split("|");
+
+  const name = names[0];
+
+  let option: "rx" | "local" | "session" | null = null;
+  if (names.length > 1) {
+    const opt = names[1];
+    if (opt === "rx" || opt === "local" || opt === "session") {
+      option = opt;
+    }
+  }
+
+  if (option === "rx") {
+    const comp = createComputation(element, expr);
+    setVar(element, name, "v" in comp ? comp : comp());
+    return;
+  }
+
+  const s = createSignal(element, expr);
+  setVar(element, name, s);
+
+  if (option !== null) {
+    const storage = option === "local" ? localStorage : sessionStorage;
+
+    const value = getItem(storage, name, typeof s.uv);
+    if (typeof value !== "undefined") {
+      s.v = value;
+      effect(() => storage.setItem(name, s.v));
+    }
   }
 }
 
